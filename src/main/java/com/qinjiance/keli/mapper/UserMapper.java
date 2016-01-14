c618d4daba7a78c0ebfa3181810b721f73d98637
qinjiance <qinjiance@126.com>
2016-01-14 18:25:08 +0800

    Update

---
+++ src/main/java/com/qinjiance/keli/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.qinjiance.keli.mapper;
+
+/**
+ * @author "Jiance Qin"
+ *
+ * @date 2016年1月14日
+ *
+ * @time 下午6:24:13
+ *
+ * @desc 
+ *
+ */
+public interface UserMapper {
+
+}
---
+++ src/main/java/com/qinjiance/keli/model/po/User.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.qinjiance.keli.model.po;
+
+import module.laohu.commons.model.BaseObject;
+
+import org.apache.ibatis.type.Alias;
+
+/**
+ * @author "Jiance Qin"
+ * 
+ * @date 2016年1月14日
+ * 
+ * @time 下午6:24:04
+ * 
+ * @desc
+ * 
+ */
+@Alias("user")
+public class User extends BaseObject {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3146715554401793947L;
+
+	/**
+	 * 
+	 */
+	public User() {
+
+	}
+
+}
